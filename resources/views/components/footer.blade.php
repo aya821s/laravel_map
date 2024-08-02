cff9039fc0701c16e1616b4349cac8913be8e8ef
aya821s <aya821cat@gmail.com>
2024-08-02 22:14:43 +0900

    Fix footer link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light fixed-bottom mt-5 footer-container">
     <div class="container-fluid d-flex justify-content-center">
-        <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-            <img src="{{asset('/images/logo_images/logo_g.png')}}" class="mr-2" style="height: 26px; margin-right: 6px;">
+        <a class="navbar-brand d-flex align-items-center" href="{{ Auth::guard('admin')->check() ? route('admin.home') : url('main') }}">
+            <img src="{{asset('/images/logo_images/logo_g.png')}}" style="height: 26px; margin-right: 6px;">
             {{ config('app.name', 'Laravel') }}
         </a>
     </div>
